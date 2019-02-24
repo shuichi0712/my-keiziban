81961996cde1e675a7cffe30b84c60ac5534948d
shu1good <shu1good@github.com>
2019-02-24 19:51:44 +0900

    新規追加コミット

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <p>
+      <button @click="nameGetArrayList">Get</button>
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {
+  public ArrayTest = [{ id: 1, name: 'callen' }, { id: 2, name: 'Tom' }, { id: 3, name: 'ohn' }];
+
+  /**
+   * nameGetArrayList
+   */
+  public nameGetArrayList() {
+    const list = this.ArrayTest.map((obj) => obj.name);
+    // console.log('list');
+  }
+
+}
+</script>
