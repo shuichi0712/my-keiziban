daf7fd71897af38b445fe57765e29b19064ec270
shu1good <shu1good@github.com>
2019-03-27 00:23:43 +0900

    todoリストのコンポーネントを追加

---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h3>
+            Todo List</h3>
+        <ul>
+            <!-- <li v-for="todo in todos" :key="todo.id">
+                    <p>ID:{{todo.id}}</p>
+                    <p>Text:{{todo.text}}</p>
+                </li> -->
+        </ul>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+export default class TodoList extends Vue {
+    @Prop({ type: String, default: [{}] })
+    public todos;
+}
+</script>
---
+++ src/components/TodoListAdd.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="bord-input">
+        <h2>Todo Input </h2>
+        <input type="text" @keyup.enter="addTodoText">
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+export default class TodoListAdd extends Vue {
+
+    public addTodoText(e) {
+        this.$store.dispatch("addTodo", { text: e.target.value });
+    }
+}
+</script>
+<style scoped>
+.bord-input {
+    margin: 0 auto;
+}
+</style>
+
